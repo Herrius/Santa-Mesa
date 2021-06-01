a12b635d7ed0d7d32bd7f8ba5e731877f96bf513
Enrique Ubaldo <65200014+Herrius@users.noreply.github.com>
2021-05-31 20:01:09 -0500

    mostrar tablas en asp.net core

---
+++ WebApplication3/Models/producto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication3.Models
+{
+    public class producto
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Modelo { get; set; }
+        public string Descripcion { get; set; }
+        public string Idioma { get; set; }
+        public string Jugadores { get; set; }
+        public string Edad { get; set; }
+    }
+}
+
