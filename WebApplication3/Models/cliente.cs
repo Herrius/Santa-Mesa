b728218e5ed4dcd57b24572df905e76f64c38ca9
fernandoUreta <72941896@continental.edu.pe>
2021-06-14 21:16:33 -0500

    fer_01
    Initial config

---
+++ WebApplication3/Models/cliente.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication3.Models
+{
+    public class Cliente
+    {
+        public int id_cliente { get; set; }
+        public string nombres { get; set; }
+        public string telefono { get; set; }
+        public string email { get; set; }
+        public string direccion { get; set; }
+        public string dni { get; set; }
+        public string ciudad { get; set; }
+    }
+}
+
