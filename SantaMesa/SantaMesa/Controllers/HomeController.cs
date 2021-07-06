381cf1c4d320fc6d6aed89e2b26154281ba7dabe
fernandoUreta <72941896@continental.edu.pe>
2021-07-06 14:09:07 -0500

    contact completo

--- SantaMesa/SantaMesa/Controllers/HomeController.cs
+++ SantaMesa/SantaMesa/Controllers/HomeController.cs
@@ -25,10 +25,52 @@ namespace SantaMesa.Controllers
             return View();
         }
 
+        public ActionResult Contacto() {
+
+            try {
+                string contenido = " El cliente" + Request.Form["nombres"].ToString() 
+                    + Request.Form["apellidos"].ToString() 
+                    + " ingresó el día " + DateTime.Now.ToString() + " "
+                    + " Nombre de empresa: " + Request.Form["nombreEmpresa"].ToString() 
+                    + " RUC: " + Request.Form["ruc"].ToString() + " Celular: " 
+                    + Request.Form["celular"].ToString() + " Email: " +
+                    Request.Form["email"].ToString() + " Solicitando la siguiete información: "
+                    + Request.Form["mensaje"].ToString();
+
+                enviarEmail(Request.Form["asunto"].ToString(), contenido);
+                return View("Index");
+            }
+            catch { }                      
+            return View("Index");
+        }
+
+        public void enviarEmail(string subject,string mensaje) {
+
+            System.Net.Mail.MailMessage mmsg = new System.Net.Mail.MailMessage();
+
+            mmsg.To.Add("76927894@continental.edu.pe");
+            mmsg.Subject = subject;
+            mmsg.SubjectEncoding = System.Text.Encoding.UTF8;
+
+            mmsg.Body = mensaje;
+            mmsg.BodyEncoding = System.Text.Encoding.UTF8;
+            mmsg.IsBodyHtml = true;
+
+            mmsg.From = new System.Net.Mail.MailAddress("fer.storetest@gmail.com");
+
+            System.Net.Mail.SmtpClient emisor = new System.Net.Mail.SmtpClient();
+            emisor.Credentials = new System.Net.NetworkCredential("fer.storetest@gmail.com", "KIPLS8899");
+            emisor.Port = 587;
+            emisor.EnableSsl = true;    
+            emisor.Host = "smtp.gmail.com";
+
+            emisor.Send(mmsg); 
+           
+        }
+
         public ActionResult Contact()
         {
             ViewBag.Message = "Your contact page.";
-
             return View();
         }
         public ActionResult Catalogo(int? id)
